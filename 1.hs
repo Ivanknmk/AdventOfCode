d41f7d1645493b4742fd496adb1c0d1a38f2eac2
Ivan Garcia <ivan.garcia@unosquare.com>
2015-12-16 08:29:20 -0800

    Segundo ejercicio

--- 1.hs
+++ 1.hs
@@ -1,12 +1,4 @@
-elevator :: Int -> Int -> Bool -> String -> (Int, Int)
-elevator position floor True [] = (position, floor)
-elevator position floor False [] = (-1, floor)
-elevator position floor basement (x:xs) =
-  let fl = if x == '('
-              then floor + 1
-              else floor - 1
-      fg = if basement then basement else fl == (-1)
-      po = if fg == False
-              then position + 1
-              else position
-  in elevator po fl fg xs
+elevator :: String -> Int
+elevator [] = 0
+elevator (x:xs) = if x == '(' then  elevator xs + 1
+                  else              elevator xs - 1
